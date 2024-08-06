e9032d5906bbb02f932b086773d331d67b544cf6
Clisdermar <clisdermar@gmail.com>
2024-08-06 00:30:55 -0400

    "Navbar, Posts, frontend "

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Post />
+      <Post imageSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvFrcsPDCAiz8MVqAb4psCnw1Arp3uXbVhKA&s" username = "Clisdermar" caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+      <Post imageSrc="https://codigoonclick.com/wp-content/uploads/2017/11/editores-codigo-javascript.jpg" username = "Clisdermar" caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+      <Post imageSrc="https://www.vtv.gob.ve/wp-content/uploads/2023/07/rio-bravo-muro-flotante.jpg" username = "Clisdermar" caption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
      
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -8,13 +8,13 @@ class Post extends Component {
             <div className="post">'
                <div className="post_interno">
                 <div className="post_header">
-                    <i className="post_avatar" class="fa-solid fa-circle-user fa-4x"></i>
-                    <h3>Username</h3>
+                    <i className="post_avatar" class="fa-solid fa-circle-user fa-3x"></i>
+                    <h3>{this.props.username}</h3>
                 </div>
 
-                <img className = "post_image" alt= "Random" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR37Y1m_hI9VvbUKHazJ2j61AJ5CcZGVWHM9A&s" />
+                <img className = "post_image" alt= {this.props.caption} src={this.props.imageSrc}/>
 
-                <h4 className="post_descripcion"><strong>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "</strong></h4>
+                <h4 className="post_descripcion"><strong>{this.props.caption}</strong></h4>
               </div>
             </div>
 
